5d87fb6125d19bac13b50a66e98f3be152ba3066
nickycarr81 <nickycarr81@hotmail.com>
2017-12-03 19:52:43 +0000

    Location Class Added - Google Maps added

--- app/src/main/java/com/example/nicky/wellness/MainActivity.java
+++ app/src/main/java/com/example/nicky/wellness/MainActivity.java
@@ -1,29 +1,85 @@
 package com.example.nicky.wellness;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.nicky.wellness.assessment.Assessment;
 import com.example.nicky.wellness.dashboard.DashBoard;
 import com.example.nicky.wellness.location.Location;
 import com.example.nicky.wellness.memberdetails.ListData;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 /**
  * Created by Nicky Carr on 10/11/2017.
  * This is the main class for the app homepage.
  * This allows navigation to different activities
- * uisnf image buttons and intents.
+ * using image buttons and intents.
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOGUE_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isServicesOK()){
+            init();
+        }
     }
+
+    private void init(){
+        ImageButton btnMap = (ImageButton) findViewById(R.id.imageButton3);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Location.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+
+    /**
+     * Function to check device has the correct
+     * google play version to view google maps
+     */
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            //User can make the map request
+            Log.d(TAG, "isServiceOK: Google Play Services are working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            // an error occurred but it can be resolved
+            Log.d(TAG, "isServiceOK: An error occurred but it can be resolved");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOGUE_REQUEST);
+            dialog.show();
+
+        }else{
+            Toast.makeText(this,"You cant make map requests",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
 
 
     /** Called when the user taps the view resource button*/
@@ -38,11 +94,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /** Called when the user taps the location button*/
+    /** Called when the user taps the location button
     public void viewLocationPage(View view){
         Intent intent = new Intent(this, Location.class);
         startActivity(intent);
-    }
+    }*/
 
     /** Called when the user taps the assessment button*/
     public void viewAssessPage(View view){
--- app/src/main/java/com/example/nicky/wellness/location/Location.java
+++ app/src/main/java/com/example/nicky/wellness/location/Location.java
@@ -1,15 +1,97 @@
 package com.example.nicky.wellness.location;
 
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.nicky.wellness.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class Location extends AppCompatActivity {
+
+public class Location extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        Toast.makeText(this,"Map is available",Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        mMap = googleMap;
+    }
+
+    private static final String TAG = "Location";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private boolean mLocationPermissionGranted = false;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        getLocationPermission();
     }
+
+    private void initMap() {
+        Log.d(TAG, "initMap: initialising map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(Location.this);
+    }
+
+
+        private void getLocationPermission(){
+            Log.d(TAG, "getLocationPermission: getting location permissions");
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION};
+
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+                if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED){
+                    mLocationPermissionGranted = true;
+                }else{
+                    ActivityCompat.requestPermissions(this, permissions,LOCATION_PERMISSION_REQUEST_CODE);
+                }
+
+            }
+
+        }
+
+        @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+            Log.d(TAG, "onRequestPermissionsResult: called");
+            mLocationPermissionGranted = false;
+
+            switch(requestCode){
+                case LOCATION_PERMISSION_REQUEST_CODE:{
+                    if(grantResults.length > 0){
+                        for (int i=0; i<grantResults.length; i++){
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                                mLocationPermissionGranted = false;
+                                Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                                return;
+                            }
+                        }
+                        Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                        mLocationPermissionGranted = true;
+                        // initialise map
+                        initMap();
+
+                    }
+                }
+            }
+        }
+
+
 }
